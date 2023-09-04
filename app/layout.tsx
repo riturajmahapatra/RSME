10eedaf2b603e186d0b76792fb13fbc1a9a71e89
riturajmahapatra <riturajmahapatra@gmail.com>
2023-09-04 13:30:31 +0530

    2nd changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ import Link from "next/link";
 const Font = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Janasevaa",
-  description: "Janasevaa for India",
+  title: "RSME Foundation",
+  description: "RSME",
 };
 
 export default async function RootLayout({
@@ -33,7 +33,7 @@ export default async function RootLayout({
             <Link href={"/Report"}>
               <button
                 type="button"
-                className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                className="text-white bg-gradient-to-br from-blue-500 to-blue-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
               >
                 Report an Issue ?
               </button>
